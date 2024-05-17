642296523b0eacfd48d9c9ae1f53e6cf5dbbae66
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-17 15:37:49 +0200

    WIP openjdk headless

--- packages/openjdk-17/build.sh
+++ packages/openjdk-17/build.sh
@@ -8,28 +8,36 @@ _COMMIT=82234f890786d49c49cf4ecbcb09c47bd9bea7ed
 TERMUX_PKG_SRCURL=https://github.com/openjdk/mobile/archive/$_COMMIT.tar.gz
 TERMUX_PKG_SHA256=5b298148a26e754120c6dfe699056d0609fc6ed92bfc858dc2ba4909ef6e791b
 TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="libjpeg-turbo, zlib"
+TERMUX_PKG_DEPENDS="libjpeg-turbo, zlib, fontconfig, giflib"
 TERMUX_PKG_BUILD_DEPENDS="cups, libandroid-shmem"
-# openjdk-17-x is recommended because X11 separation is still very experimental.
-TERMUX_PKG_RECOMMENDS="ca-certificates-java, openjdk-17-x, resolv-conf"
+TERMUX_PKG_RECOMMENDS="ca-certificates-java, resolv-conf"
 TERMUX_PKG_SUGGESTS="cups"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HAS_DEBUG=false
 
 termux_step_pre_configure() {
 	unset JAVA_HOME
+
+	# Even headless build needs X11 headers: https://bugs.openjdk.org/browse/JDK-8258465
+	termux_download https://xorg.freedesktop.org/releases/individual/lib/libX11-1.8.9.tar.xz \
+		"$TERMUX_PKG_CACHEDIR"/libX11-1.8.9.tar.xz \
+		779d8f111d144ef93e2daa5f23a762ce9555affc99592844e71c4243d3bd3262
+	cd $TERMUX_PKG_TMPDIR
+	tar xf "$TERMUX_PKG_CACHEDIR"/libX11-1.8.9.tar.xz
 }
 
 termux_step_configure() {
 	local jdk_ldflags="-L${TERMUX_PREFIX}/lib -Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-17-openjdk/lib -Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
+
 	bash ./configure \
 		--disable-precompiled-headers \
 		--disable-warnings-as-errors \
 		--enable-option-checking=fatal \
+		--enable-headless-only \
 		--openjdk-target=$TERMUX_HOST_PLATFORM \
 		--with-cups-include="$TERMUX_PREFIX/include" \
 		--with-debug-level=release \
-		--with-extra-cflags="$CFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
+		--with-extra-cflags="$CFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1 -I$TERMUX_PKG_TMPDIR/libX11-1.8.9/include" \
 		--with-extra-cxxflags="$CXXFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
 		--with-extra-ldflags="${jdk_ldflags} -Wl,--as-needed -landroid-shmem" \
 		--with-fontconfig-include="$TERMUX_PREFIX/include" \
@@ -40,8 +48,8 @@ termux_step_configure() {
 		--with-libjpeg=system \
 		--with-libpng=system \
 		--with-toolchain-type=clang \
-		--without-x \
 		--with-zlib=system \
+		--x-includes="$TERMUX_PKG_TMPDIR/libX11-1.8.9/include" \
 		AR="$AR" \
 		NM="$NM" \
 		OBJCOPY="$OBJCOPY" \
