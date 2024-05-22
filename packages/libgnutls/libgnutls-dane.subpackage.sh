f016d8252ed88cc40531df5b9527020414153b9c
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-23 01:09:17 +0200

    libgnutls: Split out libgnutls-dane subpackage
    Decrease size of bootstrap and essential dependencies

---
+++ packages/libgnutls/libgnutls-dane.subpackage.sh
@@ -0,0 +1,4 @@
+TERMUX_SUBPKG_INCLUDE="include/gnutls/dane.h lib/libgnutls-dane.so lib/pkgconfig/gnutls-dane.pc"
+TERMUX_SUBPKG_DEPENDS="libunbound"
+TERMUX_SUBPKG_DESCRIPTION="DANE protocol implementation for GnuTLS
+Commandline utilities for interfacing with the gnutls library"
