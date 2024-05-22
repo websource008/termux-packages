71849170823d4b40dda9a0cfd452915ad708d35c
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-23 01:10:09 +0200

    openjdk: Renamed from openjdk-17

--- packages/maven/build.sh
+++ packages/maven/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION="3.9.6"
 TERMUX_PKG_SRCURL=https://dlcdn.apache.org/maven/maven-3/${TERMUX_PKG_VERSION}/binaries/apache-maven-${TERMUX_PKG_VERSION}-bin.tar.gz
 TERMUX_PKG_SHA256=6eedd2cae3626d6ad3a5c9ee324bd265853d64297f07f033430755bd0e0c3a4b
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libjansi (>= 2.4.0-1), openjdk-17"
+TERMUX_PKG_DEPENDS="libjansi (>= 2.4.0-1), openjdk"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
