5f894265ee196315acd423c8304c8a79384e8772
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-22 13:46:31 +0200

    below: Fix build

--- packages/below/build.sh
+++ packages/below/build.sh
@@ -28,7 +28,7 @@ termux_step_pre_configure() {
 
 	local d p
 	for d in $CARGO_HOME/registry/src/*/libbpf-sys-*; do
-		for p in libbpf-sys-0.6.0-1-libbpf-include-linux-{compiler,types}.h.diff; do
+		for p in libbpf-sys-0.6.0-1-libbpf-include-linux-types.h.diff; do
 			patch --silent -p1 -d ${d} \
 				< "$TERMUX_PKG_BUILDER_DIR/${p}" || :
 		done
@@ -42,7 +42,7 @@ termux_step_pre_configure() {
 	done
 
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "false" ]; then
-		export CLANG=/usr/bin/clang-15
+		export CLANG=/usr/bin/clang-16
 	fi
 }
 
