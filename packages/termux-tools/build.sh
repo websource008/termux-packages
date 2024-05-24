f0c62364132fb61e719cfc37e077dd7a1929f2fa
Fredrik Fornwall <fredrik@fornwall.net>
2024-05-24 20:51:51 +0200

    termux-tools: Remove dependency on bzip2, gzip, xz-utils and dialog

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -3,20 +3,19 @@ TERMUX_PKG_DESCRIPTION="Basic system tools for Termux"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="3.0.2"
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_SRCURL=https://github.com/fornwall/termux-tools/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=de5a25655884bf2126e71d163973ef44ad8ea67420a5b326e842e955e9d99359
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
-TERMUX_PKG_BREAKS="termux-keyring (<< 1.9)"
-TERMUX_PKG_CONFLICTS="procps (<< 3.3.15-2)"
 TERMUX_PKG_SUGGESTS="termux-api"
 
 # Some of these packages are not dependencies and used only to ensure
 # that core packages are installed after upgrading (we removed busybox
 # from essentials).
-TERMUX_PKG_DEPENDS="bzip2, coreutils, curl, dash, diffutils, findutils, gawk, grep, gzip, less, procps, psmisc, sed, tar, termux-am (>= 0.8.0), termux-am-socket (>= 1.5.0), termux-exec, util-linux, xz-utils, dialog"
+TERMUX_PKG_DEPENDS="coreutils, curl, dash, diffutils, findutils, gawk, grep, less, procps, psmisc, sed, tar, termux-am, termux-am-socket, termux-exec, util-linux"
 
 # Optional packages that are distributed as part of bootstrap archives.
 TERMUX_PKG_RECOMMENDS="ed, dos2unix, inetutils, net-tools, patch, unzip"
