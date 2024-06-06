60650d36b5113cfd492f1df20d464563591e60f4
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-06 22:40:02 +0200

    Remove termux_setup_xmake

--- build-package.sh
+++ build-package.sh
@@ -123,10 +123,6 @@ source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_rust.sh"
 # shellcheck source=scripts/build/setup/termux_setup_swift.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_swift.sh"
 
-# Utility function to setup a current xmake build system.
-# shellcheck source=scripts/build/setup/termux_setup_xmake.sh
-source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_xmake.sh"
-
 # Utility function to setup a current ninja build system.
 # shellcheck source=scripts/build/setup/termux_setup_ninja.sh
 source "$TERMUX_SCRIPTDIR/scripts/build/setup/termux_setup_ninja.sh"
