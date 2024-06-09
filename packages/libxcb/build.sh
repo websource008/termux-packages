9ed26e3f93a6a7187cbbc3ce3901d9e4ffa0136d
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-09 22:53:17 +0200

    fix(libxcb): Add build dependency on python-xcbgen

--- packages/libxcb/build.sh
+++ packages/libxcb/build.sh
@@ -8,6 +8,6 @@ TERMUX_PKG_SRCURL=https://xorg.freedesktop.org/archive/individual/lib/libxcb-$TE
 TERMUX_PKG_SHA256=599ebf9996710fea71622e6e184f3a8ad5b43d0e5fa8c4e407123c88a59a6d55
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libxau, libxdmcp"
-TERMUX_PKG_BUILD_DEPENDS="xcb-proto, xorg-util-macros"
+TERMUX_PKG_BUILD_DEPENDS="xcb-proto, python-xcbgen, xorg-util-macros"
 TERMUX_PKG_RECOMMENDS="xorg-xauth"
 TERMUX_PKG_RM_AFTER_INSTALL="lib/python*"
