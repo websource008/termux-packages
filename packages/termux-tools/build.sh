ff56c75734e7822b8b4c7c60164076f8d5c414cb
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-10 00:38:33 +0200

    bump(termux-tools): 3.0.4

--- packages/termux-tools/build.sh
+++ packages/termux-tools/build.sh
@@ -1,10 +1,10 @@
-TERMUX_PKG_HOMEPAGE=https://termux.dev/
+TERMUX_PKG_HOMEPAGE=https://github.com/termux-play-store/termux-tools
 TERMUX_PKG_DESCRIPTION="Basic system tools for Termux"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION="3.0.3"
+TERMUX_PKG_VERSION="3.0.4"
 TERMUX_PKG_SRCURL=https://github.com/termux-play-store/termux-tools/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=8cd0d8886b499175ca9d04e8ff8625de2df8969166d2ed4f7167a11fad16a280
+TERMUX_PKG_SHA256=ee4a8435ece08ca9830a1f19f1715025c68cc18f04c41eec8b99b88c1aeefcbe
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_ESSENTIAL=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -21,14 +21,4 @@ TERMUX_PKG_RECOMMENDS="ed, dos2unix, inetutils, net-tools, patch, unzip"
 
 termux_step_pre_configure() {
 	autoreconf -vfi
-}
-
-termux_step_post_make_install() {
-	TERMUX_PKG_CONFFILES="$(cat "$TERMUX_PKG_BUILDDIR/conffiles")"
-}
-
-termux_step_create_debscripts() {
-	cat <<- EOF > ./preinst
-	$(cat "$TERMUX_PKG_BUILDDIR/preinst")
-	EOF
 }
