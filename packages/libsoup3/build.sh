163e72e180d111c98f3a719cbd24ffadb163a36f
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-19 14:06:42 +0200

    Add back libsoup2 for x11 packages

--- packages/libsoup3/build.sh
+++ packages/libsoup3/build.sh
@@ -1,23 +1,20 @@
-TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/action/show/Projects/libsoup
+TERMUX_PKG_HOMEPAGE=https://wiki.gnome.org/Projects/libsoup
 TERMUX_PKG_DESCRIPTION="HTTP client and server library"
 TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-# This specific package is for libsoup-2.4.
-# libsoup-3.0 is packaged as libsoup3.
-TERMUX_PKG_VERSION=2.74.3
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION="3.4.4"
 TERMUX_PKG_SRCURL=https://download.gnome.org/sources/libsoup/${TERMUX_PKG_VERSION%.*}/libsoup-${TERMUX_PKG_VERSION}.tar.xz
-TERMUX_PKG_SHA256=e4b77c41cfc4c8c5a035fcdc320c7bc6cfb75ef7c5a034153df1413fa1d92f13
-TERMUX_PKG_AUTO_UPDATE=false
-TERMUX_PKG_DEPENDS="brotli, glib, libpsl, libsqlite, libxml2, zlib"
+TERMUX_PKG_SHA256=291c67725f36ed90ea43efff25064b69c5a2d1981488477c05c481a3b4b0c5aa
+TERMUX_PKG_DEPENDS="brotli, glib, libnghttp2, libpsl, libsqlite, zlib"
+TERMUX_PKG_BUILD_DEPENDS="glib-cross"
 TERMUX_PKG_RECOMMENDS="glib-networking"
-TERMUX_PKG_BREAKS="libsoup-dev"
-TERMUX_PKG_REPLACES="libsoup-dev"
-TERMUX_PKG_DISABLE_GIR=true
+TERMUX_PKG_DISABLE_GIR=false
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dintrospection=disabled
 -Dvapi=disabled
+-Ddocs=disabled
 -Dgssapi=disabled
+-Dtests=false
 -Dtls_check=false
 -Dsysprof=disabled
 "
@@ -36,7 +33,7 @@ termux_step_pre_configure() {
 }
 
 termux_step_post_massage() {
-	local _GUARD_FILE="lib/libsoup-2.4.so"
+	local _GUARD_FILE="lib/libsoup-3.0.so"
 	if [ ! -e "${_GUARD_FILE}" ]; then
 		termux_error_exit "Error: file ${_GUARD_FILE} not found."
 	fi
