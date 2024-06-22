6c2cdaafd43be54ea1978770946caa6094ed5dd8
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-22 23:05:27 +0200

    chore(main/imlib2): Re-enable x11

--- packages/imlib2/build.sh
+++ packages/imlib2/build.sh
@@ -8,8 +8,5 @@ TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://downloads.sourceforge.net/enlightenment/imlib2-${TERMUX_PKG_VERSION}.tar.xz
 TERMUX_PKG_SHA256=cc49931a20560968a8648c9ca079085976085ea96d59a01b1e17cb55af0ffe42
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, giflib, glib, libandroid-shmem, libbz2, libcairo, libheif, libid3tag, libjpeg-turbo, libjxl, liblzma, libpng, librsvg, libtiff, libwebp, openjpeg, zlib"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
-LIBS=-landroid-shmem
---without-x
-"
+TERMUX_PKG_DEPENDS="freetype, gdk-pixbuf, giflib, glib, libandroid-shmem, libbz2, libcairo, libheif, libid3tag, libjpeg-turbo, libjxl, liblzma, libpng, librsvg, libtiff, libwebp, libx11, libxcb, libxext, openjpeg, zlib"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="LIBS=-landroid-shmem"
