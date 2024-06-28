15dc52d19159a423f5cd8b4424779c466d8cc938
Fredrik Fornwall <fredrik@fornwall.net>
2024-06-28 15:52:53 +0200

    Fix some more x11 package builds

--- x11-packages/qt5-qtquickcontrols2/build.sh
+++ x11-packages/qt5-qtquickcontrols2/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=5.15.10
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/5.15/${TERMUX_PKG_VERSION}/submodules/qtquickcontrols2-everywhere-opensource-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=97954c92fee9031ec31341812ccf53afb3c132e2b271bcef1174367fa002fc8e
 TERMUX_PKG_DEPENDS="libc++, qt5-qtbase, qt5-qtdeclarative"
-TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools"
+TERMUX_PKG_BUILD_DEPENDS="qt5-qtbase-cross-tools, qt5-qtdeclarative-cross-tools"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_NO_STATICSPLIT=true
 
