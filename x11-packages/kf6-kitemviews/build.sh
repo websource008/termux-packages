b96a1e697ecac0a1f981fec98aa4722a2926cf6f
Fredrik Fornwall <fredrik@fornwall.net>
2024-07-02 14:14:56 +0200

    Fix more x11-packages to build

--- x11-packages/kf6-kitemviews/build.sh
+++ x11-packages/kf6-kitemviews/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=d8657d155611631834a800a6fb0aac110b6baa4435de00fbd6048bd9f1a2d42f
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools, qt6-qttools-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 "
--- x11-packages/kf6-kwidgetsaddons/build.sh
+++ x11-packages/kf6-kwidgetsaddons/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL="https://download.kde.org/stable/frameworks/${TERMUX_PKG_VERSI
 TERMUX_PKG_SHA256=0e3f9f0db9a0a6ad423cb50ffe8a01beb5feef8bc7872c35b7576180e4d1844c
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, qt6-qtbase"
-TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools"
+TERMUX_PKG_BUILD_DEPENDS="extra-cmake-modules, qt6-qttools, qt6-qttools-cross-tools"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DCMAKE_SYSTEM_NAME=Linux
 "
