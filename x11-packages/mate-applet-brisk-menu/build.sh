03373245db4cf8f58260169344ee111a094f0206
Fredrik Fornwall <fredrik@fornwall.net>
2024-07-04 12:25:09 +0200

    Fix more x11 packages to build

--- x11-packages/mate-applet-brisk-menu/build.sh
+++ x11-packages/mate-applet-brisk-menu/build.sh
@@ -7,3 +7,18 @@ TERMUX_PKG_MAINTAINER="@Yisus7u7"
 TERMUX_PKG_SRCURL=http://deb.debian.org/debian/pool/main/b/brisk-menu/brisk-menu_$TERMUX_PKG_VERSION.orig.tar.xz
 TERMUX_PKG_SHA256=5a87f4dcf7365e81a571128bf0b8199eb06a6fcd7e15ec7739be0ccff1326488
 TERMUX_PKG_DEPENDS="dconf, glib, gtk3, libnotify, libx11, mate-menus, mate-panel"
+TERMUX_PKG_BUILD_DEPENDS="glib-cross"
+TERMUX_PKG_MAKE_PROCESSES=1
+
+termux_step_pre_configure() {
+	local _WRAPPER_BIN="${TERMUX_PKG_BUILDDIR}/_wrapper/bin"
+	mkdir -p "${_WRAPPER_BIN}"
+	if [[ "${TERMUX_ON_DEVICE_BUILD}" == "false" ]]; then
+		sed "s|^export PKG_CONFIG_LIBDIR=|export PKG_CONFIG_LIBDIR=${TERMUX_PREFIX}/opt/glib/cross/lib/x86_64-linux-gnu/pkgconfig:|" \
+			"${TERMUX_STANDALONE_TOOLCHAIN}/bin/pkg-config" \
+			> "${_WRAPPER_BIN}/pkg-config"
+		chmod +x "${_WRAPPER_BIN}/pkg-config"
+		export PKG_CONFIG="${_WRAPPER_BIN}/pkg-config"
+	fi
+	export PATH="${_WRAPPER_BIN}:${PATH}"
+}
