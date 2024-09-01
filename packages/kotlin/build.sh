d5b1719d1a553f31759dee592470fc150fe16c0b
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-02 00:10:50 +0200

    fix(main/kotlin): Specify kotlin.colors.enabled=false to avoid libjansi

--- packages/kotlin/build.sh
+++ packages/kotlin/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="The Kotlin Programming Language"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION="2.0.20"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/JetBrains/kotlin/releases/download/v${TERMUX_PKG_VERSION}/kotlin-compiler-${TERMUX_PKG_VERSION}.zip
 TERMUX_PKG_SHA256=5f5d2a8ad6a718a002acd0775b67a9e27035872fdbd4b0791e3cb3ea00095931
 TERMUX_PKG_AUTO_UPDATE=true
@@ -15,6 +16,14 @@ termux_step_make_install() {
 	rm -rf $TERMUX_PREFIX/opt/kotlin
 	mkdir -p $TERMUX_PREFIX/opt/kotlin
 	cp -r ./* $TERMUX_PREFIX/opt/kotlin/
+
+	# Avoid the below error due to trying to load a libc version of libjansi:
+	# Failed to load native library:jansi-2.4.0-629e7b7df22258e7-libjansi.so. osinfo: Linux/arm64
+	# java.lang.UnsatisfiedLinkError: /data/data/com.termux/files/usr/tmp/jansi-2.4.0-629e7b7df22258e7-libjansi.so:
+	# dlopen failed: library "libc.so.6" not found:
+	# needed by /data/data/com.termux/files/usr/tmp/jansi-2.4.0-629e7b7df22258e7-libjansi.so in namespace (default)
+	sed -i '$ i\JAVA_OPTS="$JAVA_OPTS -Dkotlin.colors.enabled=false"' $TERMUX_PREFIX/opt/kotlin/bin/kotlinc
+
 	for i in $TERMUX_PREFIX/opt/kotlin/bin/*; do
 		if [ ! -f "$i" ]; then
 			continue
