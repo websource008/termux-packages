5ab6e1e1c35cd0c025e79bbe1d89b11cf4382a31
Fredrik Fornwall <fredrik@fornwall.net>
2024-09-01 23:06:00 +0200

    bump(main/openjdk): 21.0.4

--- packages/openjdk/build.sh
+++ packages/openjdk/build.sh
@@ -1,63 +1,77 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/openjdk/mobile
+TERMUX_PKG_HOMEPAGE=https://openjdk.java.net
 TERMUX_PKG_DESCRIPTION="Java development kit and runtime"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=21.0
-TERMUX_PKG_REVISION=2
-TERMUX_PKG_SRCURL=git+https://github.com/termux/openjdk-mobile-termux
-TERMUX_PKG_GIT_BRANCH=termux/jdk-21
-TERMUX_PKG_DEPENDS="libiconv, libjpeg-turbo, zlib, libandroid-shmem"
-TERMUX_PKG_BUILD_DEPENDS="cups, fontconfig, giflib, libpng, libx11, libxrender, libxext, libxtst, libxrandr"
-TERMUX_PKG_RECOMMENDS="ca-certificates-java, resolv-conf"
+TERMUX_PKG_VERSION=21.0.4
+TERMUX_PKG_SRCURL=https://github.com/openjdk/jdk21u/archive/refs/tags/jdk-${TERMUX_PKG_VERSION}-ga.tar.gz
+TERMUX_PKG_SHA256=9223a0f1db1b7ee0ca480e010d6473a8be72eaae93d883fd31ef9ba6dcc41014
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_VERSION_REGEXP="\d+\.\d+\.\d+-ga"
+TERMUX_PKG_DEPENDS="libandroid-shmem, libjpeg-turbo, zlib, littlecms"
+TERMUX_PKG_BUILD_DEPENDS="cups, fontconfig, libxrandr, libxt, xorgproto"
+# openjdk-21-x is recommended because X11 separation is still very experimental.
+TERMUX_PKG_RECOMMENDS="ca-certificates-java, openjdk-21-x, resolv-conf"
 TERMUX_PKG_SUGGESTS="cups"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HAS_DEBUG=false
+# currently zgc and shenandoahgc would be auto enabled in server variant,
+# while these features is not supported on arm.
+# only leave lto here.
+__jvm_features="link-time-opt"
 
 termux_step_pre_configure() {
 	unset JAVA_HOME
 }
 
 termux_step_configure() {
-	local jdk_ldflags="-L${TERMUX_PREFIX}/lib -Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-21-openjdk/lib -Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
+	local jdk_ldflags="-L${TERMUX_PREFIX}/lib \
+		-Wl,-rpath=$TERMUX_PREFIX/lib/jvm/java-21-openjdk/lib \
+		-Wl,-rpath=${TERMUX_PREFIX}/lib -Wl,--enable-new-dtags"
 	bash ./configure \
 		--disable-precompiled-headers \
 		--disable-warnings-as-errors \
-		--enable-headless-only \
 		--enable-option-checking=fatal \
-		--openjdk-target=$TERMUX_HOST_PLATFORM \
-		--with-cups-include="$TERMUX_PREFIX/include" \
+		--with-version-pre="" \
+		--with-version-opt="" \
+		--with-jvm-variants=server \
+		--with-jvm-features="${__jvm_features}" \
 		--with-debug-level=release \
+		--openjdk-target=$TERMUX_HOST_PLATFORM \
+		--with-toolchain-type=clang \
 		--with-extra-cflags="$CFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
 		--with-extra-cxxflags="$CXXFLAGS $CPPFLAGS -DLE_STANDALONE -D__ANDROID__=1 -D__TERMUX__=1" \
 		--with-extra-ldflags="${jdk_ldflags} -Wl,--as-needed -landroid-shmem" \
+		--with-cups-include="$TERMUX_PREFIX/include" \
 		--with-fontconfig-include="$TERMUX_PREFIX/include" \
 		--with-freetype-include="$TERMUX_PREFIX/include/freetype2" \
 		--with-freetype-lib="$TERMUX_PREFIX/lib" \
+		--with-x="$TERMUX_PREFIX/include/X11" \
+		--x-includes="$TERMUX_PREFIX/include/X11" \
+		--x-libraries="$TERMUX_PREFIX/lib" \
 		--with-giflib=system \
-		--with-jvm-variants=server \
+		--with-lcms=system \
 		--with-libjpeg=system \
 		--with-libpng=system \
-		--with-toolchain-type=clang \
-		--without-x \
 		--with-zlib=system \
-		--x-includes="$TERMUX_PREFIX/include/X11" \
+		--with-vendor-name="Termux" \
 		AR="$AR" \
 		NM="$NM" \
 		OBJCOPY="$OBJCOPY" \
 		OBJDUMP="$OBJDUMP" \
 		STRIP="$STRIP" \
 		CXXFILT="llvm-cxxfilt" \
-		BUILD_CC="/usr/bin/clang-16" \
-		BUILD_CXX="/usr/bin/clang++-16" \
-		BUILD_NM="/usr/bin/llvm-nm-16" \
-		BUILD_AR="/usr/bin/llvm-ar-16" \
-		BUILD_OBJCOPY="/usr/bin/llvm-objcopy-16" \
-		BUILD_STRIP="/usr/bin/llvm-strip-16"
+		BUILD_CC="/usr/bin/clang-17" \
+		BUILD_CXX="/usr/bin/clang++-17" \
+		BUILD_NM="/usr/bin/llvm-nm-17" \
+		BUILD_AR="/usr/bin/llvm-ar-17" \
+		BUILD_OBJCOPY="/usr/bin/llvm-objcopy-17" \
+		BUILD_STRIP="/usr/bin/llvm-strip-17" \
+		$TERMUX_PKG_MAKE_PROCESSES
 }
 
 termux_step_make() {
 	cd build/linux-${TERMUX_ARCH/i686/x86}-server-release
-	make JOBS=1 images
+	make images
 }
 
 termux_step_make_install() {
@@ -78,13 +92,16 @@ termux_step_post_make_install() {
 		gzip "$manpage"
 	done
 
-	local binaries="$(find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/bin -executable -type f | xargs -I{} basename "{}" | xargs echo)"
-	for tool in $binaries; do
-		ln -sf $TERMUX_PREFIX/lib/jvm/java-21-openjdk/bin/$tool $TERMUX_PREFIX/bin/$tool
+	binaries="$(find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/bin -executable -type f | xargs -I{} basename "{}" | xargs echo)"
+	manpages="$(find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1 -name "*.1.gz" | xargs -I{} basename "{}" | xargs echo)"
+
+	cd $TERMUX_PREFIX/bin
+	for binary in $binaries; do
+		ln -s -f $TERMUX_PREFIX/lib/jvm/java-21-openjdk/bin/$binary $binary
 	done
 
-	local manpages="$(find $TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1 -name "*.1.gz" | xargs -I{} basename "{}" | xargs echo)"
+	cd $TERMUX_PREFIX/share/man/man1
 	for manpage in $manpages; do
-		ln -sf $TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1/$manpage.gz $TERMUX_PREFIX/share/man/man1/$manpage.gz
+		ln -s -f $TERMUX_PREFIX/lib/jvm/java-21-openjdk/man/man1/$manpage $manpage
 	done
 }
